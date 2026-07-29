07696baa1ef237d0fcc22a57b58cadc879a559f1
Ivan <nexacoders2@gmail.com>
2026-07-29 18:01:30 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from app.routes.colleges import router as colleges_router
 
 app = FastAPI(
     title="College Web API",
     version="1.0.0"
 )
+
+app.include_router(colleges_router)
+
 
 @app.get("/")
 async def root():
