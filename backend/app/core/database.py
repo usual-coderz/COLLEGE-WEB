345a05c290261be6407b0291d2751db36d568697
Ivan <nexacoders2@gmail.com>
2026-07-29 17:59:43 +0530

    Update database.py

---
+++ backend/app/core/database.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+
+client = AsyncIOMotorClient(MONGO_URI)
+
+db = client["college_web"]
