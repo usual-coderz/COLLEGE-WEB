f920de8a52287ab43a16b46a1da2515559a08312
Ivan <nexacoders2@gmail.com>
2026-07-29 18:00:44 +0530

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://nexacoders2_db_user:dxYh7QOdHvH6OVdd@cluster0.f4qxcbk.mongodb.net/?appName=Cluster0")
 
 client = AsyncIOMotorClient(MONGO_URI)
 
